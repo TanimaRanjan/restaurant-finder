929f193cde906486fca015a0773ee8cb9bdb9c66
TanimaRanjan <tanimaranjan@gmail.com>
2020-06-14 13:02:51 -0400

    Custom hook for yelp api

---
+++ src/hooks/useBusinesses.js
@@ -0,0 +1,36 @@
+import { useState, useEffect } from 'react'
+import yelp from '../api/yelp'
+
+export default () => {
+
+    const [ results, setResults ] = useState([])
+    const [ errorMessage , setErrorMessage ] = useState('')
+
+    const searchAPI = async (searchTerm) => {
+        try {
+            const result = await yelp.get('/search', {
+                params: {
+                    limit:50,
+                    term:searchTerm, 
+                    location:'san jose'
+                }
+            })
+            setResults(result.data.businesses)
+            setErrorMessage('')
+        } catch (error) {
+            console.log(error)
+            setErrorMessage('Something went wrong !!! ')
+        }
+    }
+
+    useEffect(() => {
+        searchAPI('')
+    }, [])
+
+
+    return { 
+        results, 
+        errorMessage, 
+        searchAPI
+    }
+}
