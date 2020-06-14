0dc95947cbf2ff0fa99b938d022b5202ac8acc6c
TanimaRanjan <tanimaranjan@gmail.com>
2020-06-14 13:01:52 -0400

    Search Bar

---
+++ src/components/SearchBar.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import { Octicons } from '@expo/vector-icons'; 
+import { View, TextInput, StyleSheet } from 'react-native'
+
+const SearchBar = ({onSearchTermChange, searchTerm, onTermSubmit}) => {
+
+    return (
+        <View style={style.viewStyle}>
+
+            <Octicons name="search" style={style.iconStyle} />
+
+            <TextInput
+                style={style.inputStyle}
+                autoCapitalize="none"
+                autoCorrect={false}
+                placeholder='Search'
+                onChangeText={onSearchTermChange}
+                value={searchTerm}
+                onEndEditing={onTermSubmit}
+
+            />
+        </View>
+    )
+}
+
+const style = StyleSheet.create({
+    inputStyle : {
+        fontSize:20,
+        flex:9
+    },
+    viewStyle: {
+        backgroundColor:'#DDD',
+        height:50,
+        marginHorizontal:20,
+        marginTop:10,
+        borderRadius:5,
+        // borderWidth:1,
+        borderColor:'black',
+        flexDirection:'row',
+        justifyContent:'space-between',
+        // alignContent:'center'
+    },
+    iconStyle : {
+        fontSize:32,
+        color:"black" ,
+        alignSelf:'center',
+        marginHorizontal:15
+    }
+})
+
+
+export default SearchBar 
